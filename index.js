2f198de394cd5f32a7eb14dc64d27357a1eb6c51
William Birkett <williebirkett@gmail.com>
2017-08-23 15:13:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var name = "Joe"
 var height = "74"
 if (name === "Susan") {
   console.log("The name is correct")
-else {
+} else {
   console.log("Expected the name to be Susan")
-}
 }
 
 // Don't worry about this
